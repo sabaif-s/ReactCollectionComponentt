b3bd109636313b72d0f3cd259e20d5da53edd687
sabaif-s <sebaifmuhammed33@gmail.com>
2024-11-25 17:08:24 +0300

    Adding Grid Animation

--- src/components/homepage/HomePageOne.jsx
+++ src/components/homepage/HomePageOne.jsx
@@ -3,26 +3,50 @@ import ReactPlayer from 'react-player';
 import { useState,useEffect } from 'react';
 
 const  HomePageOne = () => {
-    const [showBack,setShowBack]=useState(true);
+    const [showBack,setShowBack]=useState(false);
     const [showAnother,setShowAnother]=useState(false);
+    const [showContent,setShowContent]=useState(false);
     return (
-        <div className={`${showBack ? "":"hidden"} w-full absolute flex pb-10  bottom-0 `} style={{height:"calc(100vh - 50px)" }}  >
+        <div className={` w-full ${showBack ? "":"hidden"} absolute flex pb-10  bottom-0 bg-black `} style={{height:"calc(100vh - 50px)" }}  >
             <img
             onLoad={()=>{
                 setTimeout(()=>{
                     setShowBack(true);
                     setTimeout(()=>{
                         setShowAnother(true);
-                    },2000);
-                },5800);
+                        setTimeout(()=>{
+                         setShowContent(true);
+                        },2000);
+                    },2500);
+                },1000);
                 console.log("back loaded");
             }}
             src="/Grid Back.jpg" className={` ${showBack ? "":'hidden'} transition-opacity duration-1000 ease-in-out ${
           showBack ? 'opacity-100' : 'opacity-0'} w-full h-full object-cover absolute z-0`} alt="" />
+          <div className={`w-full h-full absolute z-40  flex justify-start items-center ${showAnother ? "animate-slideDown":"hidden"} `} >
+                  <div className='w-1/6 border-r-2 h-full border-r-gray-400' >
+
+                  </div>
+                  <div className='w-2/6 border-r-2 h-full border-r-gray-400' >
+ 
+</div>      
+<div className='w-2/6 border-r-2 h-full border-r-gray-400' >
+
+</div>
+<div className='w-1/6 border-r-2 h-full border-r-gray-400' >
+
+</div>
+          </div>
+          <div className={`absolute bottom-10 w-full h-full flex flex-col justify-start items-center ${showAnother ? "animate-slideRight":"hidden"} `} >
+                  <div className='w-full h-1/2 border-b-2 animate-slideRight' >
+
+                  </div>
+          </div>
           {
             showAnother && (
                 <>
-                 <div className='w-1/6  pl-6 flex flex-col z-10' >
+                 
+                 <div className={` ${showContent ? "animate-fadeIn":"opacity-0"} w-1/6    pl-6 flex flex-col z-10`} >
                      <div className='w-full h-1/2 ' >
                           <div className='w-full h-2/3  flex flex-col justify-center items-start' >
                             <div className='' ><span className='text-8xl text-white ' >. .</span></div>
@@ -32,14 +56,22 @@ const  HomePageOne = () => {
                           </div>
                      </div>
                      <div className='w-full h-1/2 text-white z-10 ' >
-                      <div className='w-full h-1/4 p-4  ' >
-                            <div>
+                      <div className='w-full h-1/4 p-4  relative' >
+                      <div className='absolute w-full h-full left-0 border-b-4 border-gray-400' >
+                      </div>
+                      <div className='absolute w-full h-full left-0 border-t-4 border-gray-600' >
+</div>
+<div className='absolute w-full h-full left-0 border-l-4 border-gray-500' >
+</div>
+<div className='absolute w-full h-full left-0 border-r-4 border-gray-600' >
+</div>
+                            <div className=''>
                                 Latest
                             </div>
                       </div>
 </div>
             </div>
-            <div className='w-2/6 text-white flex flex-col z-10' >
+            <div className={` ${showContent ? "animate-fadeIn":"opacity-0"} w-2/6 text-white flex flex-col z-10`} >
                      <div className='w-full h-1/2  pb-10 flex justify-center items-end' >
                          <div className='w-full h-1/3 bg-gray-200 flex justify-center items-center overflow-x-hidden' >
                          <h2 className="text-2xl font-semibold text-gray-700 mb-4 absolute z-10">News Feed</h2>
@@ -78,7 +110,7 @@ const  HomePageOne = () => {
 
 </div>
             </div>
-            <div className='w-2/6 text-white  flex flex-col z-10' >
+            <div className={`w-2/6 text-white  flex flex-col z-10 ${showContent ? "animate-fadeIn":"opacity-0"} `} >
                   <div className='w-full h-1/2 pb-10  flex flex-col justify-end items-center' >
                          <div className='w-2/3 h-1/3  overflow-hidden pl-2 pr-6 pb-6 mb-6' >
                               <span className='w-full word-break' >
@@ -119,7 +151,7 @@ const  HomePageOne = () => {
                                  </div>
 </div>
             </div>
-            <div className='w-1/6  z-10' >
+            <div className={` ${showContent ? "animate-fadeIn":"opacity-0"} w-1/6  z-10`} >
 
             </div>
                 </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,31 @@ module.exports = withMT({
           900: "#0c4a6e",
         },
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideDown:{
+         '0%':{transform : "translateY(-100%)"},
+         '100%':{transform : "translateY(0)"}
+        },
+        slideRight:{
+        '0%':{transform : "translateX(-100%)"},
+        '100%':{transform : "translateX(0)"}
+        },
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out ',
+        bounce: 'bounce 0.5s infinite',
+        slideDown:"slideDown 2s ease-in forwards",
+        slideRight:"slideRight 2s ease-in forwards"
+      },
+    
     },
   },
   plugins: [],
